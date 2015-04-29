54db6e1a472419a4f1dc76f341e3cb41978013b1
lockemind <pedr.amaral@gmail.com>
2015-04-29 18:53:54 +0100

    up until article update

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,6 +1,29 @@
 class ArticlesController < ApplicationController
-
+include ArticlesHelper
 	def create
+		@article = Article.new(article_params)
+	  @article.save
+	  
+	  redirect_to article_path(@article)
+	end
 
+	def destroy 
+		@article = Article.find(params[:id])
+		@article.destroy
+
+		redirect_to articles_path
+	end
+
+	def edit
+  	@article = Article.find(params[:id])
+	end
+
+	def update
+	  @article = Article.find(params[:id])
+	  @article.update(article_params)
+
+	  flash.notice = "Article '#{@article.title}' Updated!"
+
+	  redirect_to article_path(@article)
 	end
 end
